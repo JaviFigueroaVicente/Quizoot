f095fa721b00b1407d02f05e3907b87df60c57e7
Marc Santiago <marcsantiagofp@ibf.cat>
2025-03-05 17:20:29 +0100

    deleted:    database/migrations/2022_09_30_172105_create_permission_tables.php 	new file:   database/migrations/2025_03_05_161909_create_permission_tables.php 	modified:   storage/logs/laravel.log 	modified:   vendor/composer/installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '577fca38b72fc28f59cee8262917ed5ff8b774ce',
+        'reference' => '51b5ca2d4acabb951d98368b477b5a3436d460b5',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -400,7 +400,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '577fca38b72fc28f59cee8262917ed5ff8b774ce',
+            'reference' => '51b5ca2d4acabb951d98368b477b5a3436d460b5',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
