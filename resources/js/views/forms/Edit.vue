d1108d346a3a206f4a074ff931526c9d4c68a007
Marc Santiago <marcsantiagofp@ibf.cat>
2025-04-07 16:37:47 +0200

    Mostrar categorias del formulario en editar

--- resources/js/views/forms/Edit.vue
+++ resources/js/views/forms/Edit.vue
@@ -1,28 +1,29 @@
 <template>
-    <div class="main-container mb-3">
-      <!-- Sección Izquierda: Título, Descripción y Subir Imagen -->
-        <div class="container left-container" v-if="formulario">
-            <div>
-                <label for="name"></label>
-                <input class="fw-bold editable-title align-left mb-5" id="name" placeholder='Introducir Aquí El Nombre  Del Formulario' v-model="formulario.name"></input>
-            </div>
-            <div>
-                <label for="description"></label>
-                <input class="editable-description align-left mb-5" id="description" placeholder='Escribe aquí la descripción del formulario...' v-model="formulario.description"></input>
-            </div>
-            <MultiSelect v-model="formulario.category_id" :options="categoryList" display="chip" optionLabel="name" optionValue="id" placeholder="Seleccione nuevas categorías" :maxSelectedLabels="3" class="w-full md:w-80"></MultiSelect>
-            <div>
-                <img v-if="formulario && formulario.media && formulario.media.length > 0" :src="formulario.media[0].original_url" alt="User image" class="form-image">
-                <img v-else src="images/placeholder.png" alt="Placeholder" class="form-image">
-            </div>
-            <DropZone v-model="formulario.thumbnail"/>
-        </div>
-        <div v-else class="col-md-4 left-section">
-            <p>Cargando formulario...</p>
-        </div>
-    </div>
-    <button type="submit" class="btn btn-custom mt-2" @click.prevent="onFormSubmit">Actualizar Formulario</button>
+  <div class="main-container mb-3">
+    <!-- Sección Izquierda: Título, Descripción y Subir Imagen -->
+      <div class="container left-container" v-if="formulario">
+          <div>
+              <label for="name"></label>
+              <input class="fw-bold editable-title align-left mb-5" id="name" placeholder='Introducir Aquí El Nombre  Del Formulario' v-model="formulario.name"></input>
+          </div>
+          <div>
+              <label for="description"></label>
+              <input class="editable-description align-left mb-5" id="description" placeholder='Escribe aquí la descripción del formulario...' v-model="formulario.description"></input>
+          </div>
+          <MultiSelect v-model="formulario.category_id" :options="categoryList" display="chip" optionLabel="name" optionValue="id" placeholder="Seleccione nuevas categorías" :maxSelectedLabels="3" class="w-full md:w-80"></MultiSelect>
+          <div>
+              <img v-if="formulario && formulario.media && formulario.media.length > 0" :src="formulario.media[0].original_url" alt="User image" class="form-image">
+              <img v-else src="images/placeholder.png" alt="Placeholder" class="form-image">
+          </div>
+          <DropZone v-model="formulario.thumbnail"/>
+      </div>
+      <div v-else class="col-md-4 left-section">
+          <p>Cargando formulario...</p>
+      </div>
+  </div>
+  <button type="submit" class="btn btn-custom mt-2" @click.prevent="onFormSubmit">Actualizar Formulario</button>
 </template>
+
 <script setup>
 import { useRouter, useRoute } from "vue-router";
 import * as yup from "yup";
@@ -40,154 +41,155 @@ yup.setLocale(es);
 
 // Inicializar formulario
 formulario.value = {
-    name: "",
-    description: "",
-    thumbnail: null,
-    category_id: [],
+  name: "",
+  description: "",
+  thumbnail: null,
+  category_id: [],
 };
 
 const schema = yup.object().shape({
-    name: yup.string().required("El nombre es obligatorio"),
-    description: yup.string().required("La descripción es obligatoria"),
-    category_id: yup.array().min(1, "Debe seleccionar al menos una categoría"),
+  name: yup.string().required("El nombre es obligatorio"),
+  description: yup.string().required("La descripción es obligatoria"),
+  category_id: yup.array().min(1, "Debe seleccionar al menos una categoría"),
 });
 
 onMounted(async () => {
-    await getCategoryList();
-    const formularioId = route.params.id;
-    if (formularioId) {
-        const response = await getForm(formularioId);
-        formulario.value = response;
+  await getCategoryList();
+  const formularioId = route.params.id;
 
-        const categorias = await getFormCategories(formularioId);
-        console.log("Categorías del formulario:", categorias);
-    }
+  if (formularioId) {
+      const response = await getForm(formularioId);
+      formulario.value = response;
+
+      const categorias = await getFormCategories(formularioId);
+      formulario.value.category_id = categorias;
+  }
 });
 
 // Enviar formulario
 const onFormSubmit = async () => {
-    try {
-        await schema.validate(formulario.value, { abortEarly: false });
+  try {
+      await schema.validate(formulario.value, { abortEarly: false });
 
-        await updateForm(formulario.value);
+      await updateForm(formulario.value);
 
-        router.push({ name: "mis-formularios.index" });
-    } catch (validationError) {
-        console.error("Errores de validación:", validationError.errors);
-    }
+      router.push({ name: "mis-formularios.index" });
+  } catch (validationError) {
+      console.error("Errores de validación:", validationError.errors);
+  }
 };
 </script>
 
 <style scoped>
 .main-container {
-  display: flex;
-  gap: 20px;
-  justify-content: center;
-  align-items: flex-start;
+display: flex;
+gap: 20px;
+justify-content: center;
+align-items: flex-start;
 }
 
 .left-container {
-  flex: 1;
-  text-align: center;
+flex: 1;
+text-align: center;
 }
 
 .right-container {
-  flex: 2;
+flex: 2;
 }
 
 .container {
-  max-width: 600px;
-  background: white;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  margin-top: 20px;
+max-width: 600px;
+background: white;
+padding: 20px;
+border-radius: 10px;
+box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+margin-top: 20px;
 }
 
 .editable-title,
 .editable-description {
-  cursor: text;
-  outline: none;
-  min-height: 40px;
-  padding: 8px;
-  border: 2px solid #402462;
-  border-radius: 8px;
-  border-color: #d0d0d0;
-  background-color: #fff;
+cursor: text;
+outline: none;
+min-height: 40px;
+padding: 8px;
+border: 2px solid #402462;
+border-radius: 8px;
+border-color: #d0d0d0;
+background-color: #fff;
 }
 
 .editable-title:focus,
 .editable-description:focus {
-  border-color: #874eca;
+border-color: #874eca;
 }
 
 
 .align-left {
-  text-align: left;
+text-align: left;
 }
 
 .preview-image {
-  width: 100%;
-  max-height: 200px;
-  object-fit: cover;
-  border-radius: 10px;
-  margin-top: 10px;
+width: 100%;
+max-height: 200px;
+object-fit: cover;
+border-radius: 10px;
+margin-top: 10px;
 }
 
 .question-title {
-  font-weight: bold;
-  font-style: italic;
-  margin-top: 15px;
+font-weight: bold;
+font-style: italic;
+margin-top: 15px;
 }
 
 .answer-container {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  margin-bottom: 10px;
-  padding: 5px;
-  border-radius: 5px;
+display: flex;
+align-items: center;
+gap: 10px;
+margin-bottom: 10px;
+padding: 5px;
+border-radius: 5px;
 }
 
 .correct-answer {
-  background-color: #d4edda;
+background-color: #d4edda;
 }
 
 .incorrect-answer {
-  background-color: #f8d7da;
+background-color: #f8d7da;
 }
 
 .icon {
-  font-size: 20px;
-  cursor: pointer;
+font-size: 20px;
+cursor: pointer;
 }
 
 .hidden-radio {
-  display: none;
+display: none;
 }
 
 .btn-custom {
-  background-color: #874eca;
-  color: white;
-  border: none;
-  padding: 10px;
-  width: 100%;
-  border-radius: 5px;
-  transition: background-color 0.3s ease;
+background-color: #874eca;
+color: white;
+border: none;
+padding: 10px;
+width: 100%;
+border-radius: 5px;
+transition: background-color 0.3s ease;
 }
 .btn-custom:hover {
-  background-color: #402462;
+background-color: #402462;
 }
 
 .btn-light {
-  width: 100%;
-  margin-top: 10px;
+width: 100%;
+margin-top: 10px;
 }
 
 .form-image {
-  width: auto;
-  height: 100px;
-  object-fit: cover;
-  border-radius: 10px;
+width: auto;
+height: 100px;
+object-fit: cover;
+border-radius: 10px;
 }
 </style>
