523b0390d871ffb301e568c1de7c4dc1fdc36da7
JaviFigueroaVicente <javierfigueroa969@gmail.com>
2025-04-11 19:50:58 +0200

    pruebas unitarias

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Formularios;
+use Illuminate\Routing\Route;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
