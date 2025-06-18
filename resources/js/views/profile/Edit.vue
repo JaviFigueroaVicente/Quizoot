762ec0e022e26ca4c72a5fbb5ee5f84b62b2e720
Javier Figueroa <javierfigueroa969@gmail.com>
2025-06-18 20:20:18 +0200

    edit perfil

--- resources/js/views/profile/Edit.vue
+++ resources/js/views/profile/Edit.vue
@@ -6,8 +6,44 @@
                     <h5 class="float-start titles">PROFILE IMAGE</h5>
                 </div>
                 <div class="image-container">
-                    <img :src="usuario.avatar || '/images/Nav/PerfilSinFoto.webp'" alt="Profile Image" class="profile-image" />
-                    <DropZone v-model="imagenFile" />
+                    <!-- <img :src="usuario.avatar || '/images/Nav/PerfilSinFoto.webp'" alt="Profile Image" class="profile-image" /> -->
+                    <FileUpload
+                        name="picture"
+                        @before-upload="onBeforeUpload"
+                        @upload="onTemplatedUpload($event)"
+                        accept="image/*"
+                        :maxFileSize="1500000"
+                        @select="onSelectedFiles"
+                        pt:content:class="fu-content"
+                        pt:buttonbar:class="fu-header"
+                        pt:root:class="fu"
+                        class="fu"
+                        v-model="imagenFile"
+                        >
+
+                        <template #header="{ chooseCallback, uploadCallback, clearCallback, files, uploadedFiles }">
+                            <div class="flex flex-wrap justify-content-between align-items-center flex-1 gap-2">
+                                <div class="flex gap-2">
+                                    <Button class="boton" @click="chooseCallback()" icon="pi pi-images" rounded outlined></Button>
+                                    <Button class="boton" @click="uploadEvent(uploadCallback, uploadedFiles)" icon="pi pi-cloud-upload" rounded outlined severity="success" :disabled="!files || files.length === 0"></Button>
+                                    <Button class="boton" @click="clearCallback()" icon="pi pi-times" rounded outlined severity="danger" :disabled="!files || files.length === 0"></Button>
+                                </div>
+                                <p class="mt-4 mb-0">Arrastra y suelta la imagen aquí.</p>
+                            </div>
+                        </template>
+
+                        <template #content="{ files, uploadedFiles, removeUploadedFileCallback, removeFileCallback }">
+                            <img v-if=" files.length > 0" v-for="(file, index) of files" :key="file.name + file.type + file.size" role="presentation" :alt="file.name" :src="file.objectURL" class="object-fit-cover profile-image mt-3"/>
+                            <div v-else>
+                                <img v-if="uploadedFiles.length > 0" :key="uploadedFiles[uploadedFiles.length-1].name + uploadedFiles[uploadedFiles.length-1].type + uploadedFiles[uploadedFiles.length-1].size" role="presentation" :alt="uploadedFiles[uploadedFiles.length-1].name" :src="uploadedFiles[uploadedFiles.length-1].objectURL" class="object-fit-cover w-100 h-100 profile-image" />
+                            </div>
+                        </template>
+
+                        <template #empty>
+                            <img v-if="usuario.avatar" :src=usuario.avatar alt="Avatar" class="object-fit-cover profile-image">
+                            <img v-if="!usuario.avatar" src="/images/Nav/PerfilSinFoto.webp" alt="Avatar Default" class="object-fit-cover profile-image">
+                        </template>
+                    </FileUpload>
                 </div>
             </div>
             <button type="submit" @click="onFormSubmitIMG" class="btn-custom w-100 mt-5">
@@ -51,162 +87,13 @@
     </div> 
     <Toast />
 </template>
-
-<style scoped>   
-    .label{
-        color: #ffffff;
-    }
-    .input-form {
-        position: relative;
-    }
-    .form-control {
-        border: solid 1.5px #9e9e9e !important;
-        background: #e8e8e850;
-        padding: 1rem;
-        font-size: 1rem;
-        color: #f5f5f5;
-        transition: border 150ms cubic-bezier(0.4,0,0.2,1);
-    }
-
-    .form-label {
-        position: absolute;
-        left: 15px;
-        color: #e8e8e8;
-        pointer-events: none;
-        transform: translateY(-2.5rem);
-        transition: 150ms cubic-bezier(0.4,0,0.2,1);
-    }
-
-    .form-control:focus, .form-control:not(:placeholder-shown) {
-        outline: none;
-        border: 1.5px solid #874eca;
-        box-shadow: none;
-    }
-
-    .form-control:focus ~ label, .form-control:not(:placeholder-shown) ~ label {
-        transform: translateY(-280%) scale(0.8) !important;
-        background-color: transparent;
-        padding: 0 .2em;
-        color: #ffffff;
-        box-shadow: none;
-    }
-
-    button{
-        padding: 10px 30px;
-        box-shadow: 0px 6px 0px 0px rgba(0, 0, 0, 0.3);
-        transition: all 0.2s ease-in-out;
-    }
-
-    button:hover{
-        box-shadow: 0px 3px 0px 0px rgba(0, 0, 0, 0.3);
-        transform: translateY(4px);
-    }
-
-    h5{
-        color: #ffffff !important;
-    }
-    .titles {
-        color: #874eca;
-        font-weight: bold;
-        font-size: 1.5rem;
-    }
-    .card {
-        width: 50%;
-        margin-top: 10px;
-        background-color: transparent;
-        border: none;
-        justify-content: center;
-    }
-
-    .card-body{        
-        background-color: #874eca;
-        border-radius: 10px;
-        padding: 20px !important;
-    }
-
-    .card-main{
-        box-shadow: 2px 5px 0px 0px rgba(0, 0, 0, 0.4);
-    }
-
-    .image{
-        margin: 15px;
-    }
-
-    .image-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
-
-    .profile-image {
-        width: 150px;
-        height: 150px;
-        object-fit: cover;
-        border-radius: 50%;
-        margin-bottom: 80px;
-        border: 2px solid #ccc;
-    }
-
-    .main-container {
-        display: flex;
-        gap: 20px;
-        justify-content: center;
-        align-items: flex-start;
-    }
-
-    .left-container {
-        flex: 1;
-    }
-
-    .right-container {
-        flex: 2;
-    }
-
-    @media (max-width: 768px) {
-        .main-container {
-            flex-direction: column;
-            gap: 10px;
-        }
-
-        .left-container,
-        .right-container {
-            flex: none;
-            width: 100%;
-        }
-
-        .right-container {
-            width: 100%;
-        }
-
-        .profile-image {
-            width: 120px;
-            height: 120px;
-            margin-bottom: 30px;
-        }
-    }
-    
-    @media (max-width: 376px) {
-        .profile-image {
-            width: 100px;
-            height: 100px;
-            margin-bottom: 40px;
-        }
-    }
-            
-</style>
-
-
 <script setup>
-import { onMounted, reactive, watchEffect,ref, inject } from "vue";
+import { onMounted, reactive, watchEffect, ref, inject } from "vue";
 // import { useField, defineRule } from "vee-validate";
-import { required, min } from "@/validation/rules";
 import { authStore } from '@/store/auth';
 import { useRoute, useRouter } from "vue-router";
 import * as yup from "yup";
 import { es } from "yup-locales";
-import DropZone from "@/components/DropZone.vue";
 
 yup.setLocale(es);
 const route = useRoute()
@@ -258,16 +145,6 @@ const updateUser = async () => {
 
 // Función para actualizar solo la imagen
 const updateImage = async () => {
-    if (!imagenFile.value) {
-        toast.add({
-            severity: 'error',
-            summary: 'Error',
-            detail: 'Debe seleccionar una imagen',
-            life: 3000,
-        });
-        return;
-    }
-
     // Crear FormData para enviar el archivo
     const formData = new FormData();
     formData.append('users', imagenFile.value);
@@ -334,6 +211,7 @@ const onFormSubmit = () => {
 
 // Validación y envío del formulario de imagen
 const onFormSubmitIMG = () => {
+    console.log(imagenFile.value);
     if (imagenFile.value) {
         updateImage();
         // window.location.reload();
@@ -357,6 +235,36 @@ onMounted(() => {
         })
 })
 
+const totalSize = ref(0);
+const totalSizePercent = ref(0);
+
+const onBeforeUpload = (event) => {
+    // console.log('onBeforeUpload')
+    event.formData.append('id', user.id)
+};
+
+const uploadEvent = async (callback, uploadedFiles) => {
+    console.log('uploadEvent');
+    totalSizePercent.value = totalSize.value / 10;
+    await callback();
+};
+
+const onSelectedFiles = (event) => {
+     if (event.files.length === 0) {
+        imagenFile.value = null;
+        return;
+    }
+
+    // Tomamos solo el primer archivo
+    const selectedFile = event.files[0];
+    imagenFile.value = selectedFile;
+
+    // Calcular tamaño total
+    totalSize.value = selectedFile.size;
+    totalSizePercent.value = (selectedFile.size / 1500000) * 100;
+
+    console.log('Archivo asignado a imagenFile:', imagenFile.value);
+};
 
 // defineRule('required', required);
 // defineRule('min', min);
@@ -430,3 +338,172 @@ onMounted(() => {
 //     profile.email = store.user.email || '';
 // });
 </script>
+
+
+<style scoped>   
+    .label{
+        color: #ffffff;
+    }
+    .input-form {
+        position: relative;
+    }
+    .form-control {
+        border: solid 1.5px #9e9e9e !important;
+        background: #e8e8e850;
+        padding: 1rem;
+        font-size: 1rem;
+        color: #f5f5f5;
+        transition: border 150ms cubic-bezier(0.4,0,0.2,1);
+    }
+
+    .form-label {
+        position: absolute;
+        left: 15px;
+        color: #e8e8e8;
+        pointer-events: none;
+        transform: translateY(-2.5rem);
+        transition: 150ms cubic-bezier(0.4,0,0.2,1);
+    }
+
+    .form-control:focus, .form-control:not(:placeholder-shown) {
+        outline: none;
+        border: 1.5px solid #874eca;
+        box-shadow: none;
+    }
+
+    .form-control:focus ~ label, .form-control:not(:placeholder-shown) ~ label {
+        transform: translateY(-280%) scale(0.8) !important;
+        background-color: transparent;
+        padding: 0 .2em;
+        color: #ffffff;
+        box-shadow: none;
+    }
+
+    button{
+        padding: 10px 30px;
+        box-shadow: 0px 6px 0px 0px rgba(0, 0, 0, 0.3);
+        transition: all 0.2s ease-in-out;
+    }
+
+    button:hover{
+        box-shadow: 0px 3px 0px 0px rgba(0, 0, 0, 0.3);
+        transform: translateY(4px);
+    }
+
+    h5{
+        color: #ffffff !important;
+    }
+
+    p{
+        color: #ffffff !important;
+    }
+
+    .titles {
+        color: #874eca;
+        font-weight: bold;
+        font-size: 1.5rem;
+    }
+    .card {
+        width: 50%;
+        margin-top: 10px;
+        background-color: transparent;
+        border: none;
+        justify-content: center;
+    }
+
+    .card-body{        
+        background-color: #874eca;
+        border-radius: 10px;
+        padding: 20px !important;
+    }
+
+    .card-main{
+        box-shadow: 2px 5px 0px 0px rgba(0, 0, 0, 0.4);
+    }
+
+    .image{
+        margin: 15px;
+    }
+
+    .image-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
+
+    .profile-image {
+        width: 150px;
+        height: 150px;
+        object-fit: cover;
+        border-radius: 50%;
+        margin-bottom: 20px;
+        border: 2px solid #ccc;
+    }
+
+    .main-container {
+        display: flex;
+        gap: 20px;
+        justify-content: center;
+        align-items: flex-start;
+    }
+
+    .left-container {
+        flex: 1;
+    }
+
+    .right-container {
+        flex: 2;
+    }
+
+    .fu-content {
+        padding: 0px !important;
+        border: 0px !important;
+        border-radius: 6px;
+    }
+
+    .fu-header {
+        border: 0px !important;
+        border-radius: 6px;
+    }
+
+    .fu {
+        display: flex;
+        flex-direction: column-reverse;
+        align-items: center;
+        border-radius: 6px;
+        background-color: transparent;
+        border: none;
+        padding-top: 30px;
+    }
+
+    .boton{
+        background: #e8e8e8 !important;
+    }
+
+    @media (max-width: 768px) {
+        .main-container {
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        .left-container,
+        .right-container {
+            flex: none;
+            width: 100%;
+        }
+
+        .right-container {
+            width: 100%;
+        }
+
+        .profile-image {
+            width: 120px;
+            height: 120px;
+        }
+    }
+
+            
+</style>
+
